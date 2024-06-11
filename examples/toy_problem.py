e92a81e4d7bbcdd8663ff9bda4a03efaf04e516a
May Meta <may42meta@gmail.com>
2024-06-11 12:57:06 +0300

    add demonstrate_recoverability to toy_problem.py

--- examples/toy_problem.py
+++ examples/toy_problem.py
@@ -21,9 +21,41 @@ def solve_toy_problem_with_fsm(example_inputs: str, on_output: Callable, verbose
         print(f'final state: {my_fsm.current_state!r}, final output: {my_fsm.current_output!r}')
 
 
+def demonstrate_recoverability(
+        example_inputs: str,
+        more_inputs: str,
+        on_output: Callable,
+        fsm_filename: str = 'toy_problem_fsm.json',
+) -> None:
+    my_fsm: MooreFSM = streak_detector(
+        ['S', 'L'], n_streak=3, streak_output_template='Error! Too many {input_value} lollipops!'
+    )
+    for my_inp in example_inputs:
+        my_out = my_fsm.transition(my_inp)
+        my_fsm.to_json(fsm_filename)
+        if my_out is not None:
+            on_output(my_out)
+    print('Worker stopped, deleting my_fsm...')
+    del my_fsm
+    print(f'Restoring from {fsm_filename!r}...')
+    restored_fsm = MooreFSM.from_json(fsm_filename)
+    for my_inp in more_inputs:
+        my_out = restored_fsm.transition(my_inp)
+        restored_fsm.to_json(fsm_filename)
+        if my_out is not None:
+            on_output(my_out)
+
+
 if __name__ == '__main__':
+    print('demonstrating streak detection:')
     solve_toy_problem_with_fsm(
         example_inputs='SLSLSLSSLLSSLLSSSLLLSSSSLLLLSSSSSSSS',
         on_output=print,
         verbose=True,
+    )
+    print('\ndemonstrating recoverability:')
+    demonstrate_recoverability(
+        example_inputs='SLSLLLS',
+        more_inputs='SSLSLSLLLS',
+        on_output=print,
     )
